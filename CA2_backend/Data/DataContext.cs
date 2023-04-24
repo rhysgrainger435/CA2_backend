35fa63317d9d5ed3dc02b7c1140a02fd50e36c65
conor2019 <71699812+conor2019@users.noreply.github.com>
2023-04-24 16:58:08 +0100

    DB populated and connected

--- CA2_backend/Data/DataContext.cs
+++ CA2_backend/Data/DataContext.cs
@@ -9,5 +9,81 @@ namespace CA2_backend.Data
 
 		public DbSet<Songs> Songs { get; set; }
 		public DbSet<Artist> Artists { get; set; }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<Songs>()
+				.Property(s => s.id).HasColumnName("id");
+
+			modelBuilder.Entity<Songs>()
+				.Property(s => s.songName).HasColumnName("name");
+
+			modelBuilder.Entity<Songs>()
+				.Property(s => s.artistName).HasColumnName("artistName");
+
+			modelBuilder.Entity<Songs>()
+				.Property(s => s.albumName).HasColumnName("albumName");
+
+			modelBuilder.Entity<Artist>()
+				.Property(a => a.id).HasColumnName("id");
+
+			modelBuilder.Entity<Artist>()
+				.Property(a => a.artistName).HasColumnName("artistName");
+
+			modelBuilder.Entity<Artist>()
+				.Property(a => a.genre).HasColumnName("genre");
+
+			modelBuilder.Entity<Songs>()
+				.ToTable("songs");
+
+			modelBuilder.Entity<Artist>()
+				.ToTable("artists");
+
+			// Hardcoding data to the DB
+			modelBuilder.Entity<Songs>().HasData(
+				new Songs
+				{
+					id = 1,
+					songName = "Law Of Attraction",
+					artistName = "Dave",
+					albumName = "We're All Alone In This Together"
+				},
+				new Songs
+				{
+					id = 2,
+					songName = "Ferrari Horses",
+					artistName = "D Block Europe",
+					albumName = "The Blue Print - Us Vs. Them"
+				},
+				new Songs
+				{
+					id = 3,
+					songName = "One More Time",
+					artistName = "Daft Punk",
+					albumName = "Discovery"
+				}
+			);
+
+			modelBuilder.Entity<Artist>().HasData(
+				new Artist
+				{
+					id = 1,
+					artistName = "Dave",
+					genre = "Pop"
+				},
+				new Artist
+				{
+					id = 2,
+					artistName = "D Block Europe",
+					genre = "Rap"
+				},
+				new Artist
+				{
+					id = 3,
+					artistName = "Daft Punk",
+					genre = "Dance"
+				}
+			);
+		}
 	}
 }
--- CA2_backend/Program.cs
+++ CA2_backend/Program.cs
@@ -1,5 +1,7 @@
 global using CA2_backend.Data;
 global using Microsoft.EntityFrameworkCore;
+global using Microsoft.EntityFrameworkCore.Sqlite;
+using CA2_backend;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,13 +10,73 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddDbContext<DataContext>(options =>
 {
-	options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+	//options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+	options.UseSqlite("Data Source=songs.db");
 });
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+	var dbContext = scope.ServiceProvider.GetService<DataContext>();
+	dbContext.Database.EnsureCreated();
+
+	if (!dbContext.Songs.Any())
+	{
+		// Seed the data to the database
+		dbContext.Songs.AddRange(new[]
+		{
+			new Songs
+			{
+				id = 1,
+				songName = "Law Of Attraction",
+				artistName = "Dave",
+				albumName = "We're All Alone In This Together"
+			},
+			new Songs
+			{
+				id = 2,
+				songName = "Ferrari Horses",
+				artistName = "D Block Europe",
+				albumName = "The Blue Print - Us Vs. Them"
+			},
+			new Songs
+			{
+				id = 3,
+				songName = "One More Time",
+				artistName = "Daft Punk",
+				albumName = "Discovery"
+			}
+		});
+
+		dbContext.Artists.AddRange(new[]
+		{
+			new Artist
+			{
+				id = 1,
+				artistName = "Dave",
+				genre = "Pop"
+			},
+			new Artist
+			{
+				id = 2,
+				artistName = "D Block Europe",
+				genre = "Rap"
+			},
+			new Artist
+			{
+				id = 3,
+				artistName = "Daft Punk",
+				genre = "Dance"
+			}
+		});
+
+		dbContext.SaveChanges();
+	}
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
